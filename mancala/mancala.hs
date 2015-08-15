9b97431882896ef78d1f8fc5a07adb64495c218e
白井 よう <Yo@MacBook.local>
2015-08-16 02:39:16 +0900

    complete display part

--- mancala/mancala.hs
+++ mancala/mancala.hs
@@ -7,6 +7,8 @@ main :: IO()
 main = do
 	displayBoard
 	
+myboard = (4, 12, 3, 5, 0, 4)
+
 -- game board
 --  -------------------------------
 -- | ( 4) ( 4) ( 4) ( 4) ( 4) ( 4) |
@@ -16,10 +18,17 @@ main = do
 displayBoard :: IO()
 displayBoard = do
 	putStrLn "  ------------------------------- "
-	putStrLn " | ( 4) ( 4) ( 4) ( 4) ( 4) ( 4) |"
+	putStrLn $ " |" ++ (createMyBoard myboard) ++ " |" 
 	putStrLn " |                               |"
-	putStrLn " | ( 4) ( 4) ( 4) ( 4) ( 4) ( 4) |"
+	putStrLn $ " |" ++ (createMyBoard myboard) ++ " |" 
 	putStrLn "  ------------------------------- "
 
-createMyBoard :: Integral a => [a] -> (a,a,a,a,a,a)
-createMyBoard [] = (0,0,0,0,0,0) 
+createMyBoard :: (Integral a, Show a) => (a,a,a,a,a,a) -> [Char]
+createMyBoard (a1,a2,a3,a4,a5,a6) = concat $ map createHouse [a1,a2,a3,a4,a5,a6] 
+
+createHouse :: (Integral a, Show a) => a -> [Char]
+createHouse a
+	| a <= 0 = " ( 0)"
+	| a < 10 = " ( " ++ (show a) ++ ")"
+	| a < 100 = " (" ++ (show a) ++ ")"
+	| otherwise = " (99)"
