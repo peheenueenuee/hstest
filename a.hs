6e4498d811c9cbf060c204a7bfada7afba2ebeac
shirai <shirai@cc-arc.co.jp>
2015-08-21 14:46:16 +0900

    add mancala logic mockup

--- a.hs
+++ a.hs
@@ -3,6 +3,43 @@ vava = [(1,2),(3,4),(5,6)]
 pop = [1,2,3]
 inputValue = 150
 
+smphouses = [1,2,3,4,5,6,1,2,3,4,5]
+smphouses3 = [4,5,6,1,2,3,4,5,6,1,2]
+smphouses4 = [5,6,1,2,3,4,5,6,1,2,3]
+smpboard = ((1,2,3,4,5,6), (1,2,3,4,5,6))
+smpmyboard = (1,2,3,4,5,6)
+
+spreadSeeds :: Integral a => [a] -> [a]
+spreadSeeds [] = []
+spreadSeeds (x:xs)
+	| x <= 0 = xs
+	| x <= fromIntegral(length xs) = map (\ n -> n + 1) (fst (splitAt x2int xs)) ++ ( snd (splitAt x2int xs))
+	| otherwise = spreadSeeds $ (x - fromIntegral(length xs)): (map (\ n -> n + 1) xs)
+	where
+		x2int = (fromIntegral x ::Int)
+
+distructBoard :: (Integral b) => Int -> ((b,b,b,b,b,b), (b,b,b,b,b,b)) -> [b]
+distructBoard a (myboard, opoboard) = myboard_tail ++ (taple2list opoboard) ++ myboard_head
+	where
+		myboard_head = take (a-1) $ taple2list myboard
+		myboard_tail = snd $ splitAt (a-1) $ taple2list myboard
+
+taple2list :: (b,b,b,b,b,b) -> [b]
+taple2list (b1,b2,b3,b4,b5,b6) = [b1,b2,b3,b4,b5,b6]
+
+list2taple :: [b] -> (b,b,b,b,b,b)
+list2taple (a1:a2:a3:a4:a5:a6:xs) = (a1,a2,a3,a4,a5,a6)
+
+constructBoard :: Integral b => Int -> [b] -> ((b,b,b,b,b,b), (b,b,b,b,b,b))
+constructBoard a houselist = (list2taple (myhouses_head ++ (0:myhouses_tail)), list2taple opohouses)
+	where
+		opohouses = take 6 $ snd $ splitAt (6-a) houselist
+		myhouses_tail = take (6-a) houselist
+		myhouses_head = snd $ splitAt 6 $ snd $ splitAt (6-a) houselist
+ 
+
+goisi :: Integral a => a -> [(a, a, a)]
+goisi 0 = []
 goisi univ = [(a, b, c) | a <- [1..univ], b <- [1..((univ + 1) `div` (a + 1))], c <- [a]]
 
 triangles = [(a, b, c) | c <- [1..inputValue], a <- [1..c], b <- [1..a]]
@@ -44,7 +81,7 @@ eldrazi n
 
 raiseOfSwamp::[a] -> [a]
 raiseOfSwamp [] = []
-raiseOfSwamp oop@(x:_) = oop ++ [x] 
+raiseOfSwamp oop@(_:xs) = oop ++ xs
 
 funcConnect::Fractional a => a -> a
 funcConnect x = x * 2.02
