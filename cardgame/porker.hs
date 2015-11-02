d4bf55051af3e188013c6f6c380009f7efb42103
shirai <shirai@cc-arc.co.jp>
2015-11-02 11:58:25 +0900

    commit.

--- cardgame/porker.hs
+++ cardgame/porker.hs
@@ -22,16 +22,21 @@ getHand deck = do
 	return (hand, drop 5 deck)
 
 getDiscardList :: Hand -> IO (Maybe DiscardList)
-getDiscardList = undefined
+getDiscardList h = do
+	input <- getLine
+	return $ Just $ selectByIndice (fromHand h) (toIntList input)
+
+toIntList :: String -> Maybe [Int]
+toIntList = map $ read. (:[])
+
+selectByIndice :: [a] -> [Int] -> Maybe [a]
+selectByIndice l = map ((l!!) . (subtract 1))
 
 drawHand :: Deck -> DiscardList -> Hand -> Maybe (Hand, Deck)
 drawHand deck dis h = let
 	nl = filter (flip notElem dis) (fromHand h)
 	nr = drop (5 - (length nl)) deck
-	in do
-		newhand <- toHand . take 5 $ nl ++ deck
-		newdeck <- return nr
-		return (newhand, newdeck)
+	in (,) <$> toHand (take 5 $ nl ++ deck) <*> return nr
 
 randomHand :: IO (Maybe Hand)
 randomHand = do
