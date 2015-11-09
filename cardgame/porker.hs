d5daf72491282faaa0e484d0a80789e9913392a5
shirai <shirai@cc-arc.co.jp>
2015-11-09 19:47:35 +0900

    progress Day 5

--- cardgame/porker.hs
+++ cardgame/porker.hs
@@ -3,11 +3,28 @@ import System.Random.Shuffle
 import Cards
 import Hands
 import Data.List
+import Data.Char
 import Control.Monad
 import Control.Applicative
+import Safe
 
 main :: IO ()
 main = do
+	putStrLn "---------------------"
+	putStrLn "--- Simple Porker ---"
+	putStrLn "---------------------"
+	deck <- shuffleM allCards
+	case getHand deck of
+		Nothing -> error "Unspecified ERROR"
+		Just (hand, deck_) -> playPorker hand deck_
+-- 	ynQuestion "--- Play again?" main 
+	(putStrLn "--- Bye.")
+
+playPorker :: Hand -> Deck -> IO ()
+playPorker h0 d0 = putStrLn "(porker)"
+
+handsTest :: IO ()
+handsTest = do
 	forM_ [1..500] $ \i -> do
 		hand <- randomHand
 		res <- return $ judgePorker hand
@@ -22,15 +39,23 @@ getHand deck = do
 	return (hand, drop 5 deck)
 
 getDiscardList :: Hand -> IO (Maybe DiscardList)
-getDiscardList h = do
+getDiscardList h = do -- IO monad
 	input <- getLine
-	return $ Just $ selectByIndice (fromHand h) (toIntList input)
+	return $ do -- Maybe monad
+		intList <- toIntList input
+		res <- selectByIndice (fromHand h) intList
+		return res
 
 toIntList :: String -> Maybe [Int]
-toIntList = map $ read. (:[])
+toIntList str = if (and (map isDigit str))
+	then Just (reads str)
+	else Nothing 
+	where
+		reads :: String -> [Int]
+		reads = map $ read. (:[])
 
 selectByIndice :: [a] -> [Int] -> Maybe [a]
-selectByIndice l = map ((l!!) . (subtract 1))
+selectByIndice l = sequence . map ((atMay l) . (subtract 1))
 
 drawHand :: Deck -> DiscardList -> Hand -> Maybe (Hand, Deck)
 drawHand deck dis h = let
